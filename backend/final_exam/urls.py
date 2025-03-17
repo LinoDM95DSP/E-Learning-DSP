5317da7efcca00a1bf72ffd9e94797694e4b8aba
LinoDM95 <linodemarco95@gmail.com>
2025-03-17 09:58:32 +0100

    Django structure set up (Django app: users serves as structure template)

---
+++ backend/final_exam/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+
+urlpatterns = [
+    #path('', course_list, name='course_list'),
+    #path('<int:course_id>/', course_detail, name='course_detail'),
+]
---
+++ backend/users/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+import views as user_views
+
+urlpatterns = [
+    #path('', course_list, name='course_list'),
+    #path('<int:course_id>/', course_detail, name='course_detail'),
+]
